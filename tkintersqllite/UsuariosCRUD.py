caa06c03872de446d1ef19cc404129846f75e374
ElVittor <93359780+ElVittor@users.noreply.github.com>
2023-03-24 15:50:32 -0600

    Terminamos de crear la clase controlador
    agregamos metodos guardar, encriiptar y los ejecutamos en la clase principal, confirmamos que se conecten con la BD

--- tkintersqllite/UsuariosCRUD.py
+++ tkintersqllite/UsuariosCRUD.py
@@ -1,6 +1,16 @@
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
+#despues de crear los metodos de controladorBD hacemos lo siguiente
+from controladorBD import *
+#Para poder conectar con controladorBD vamos a crear una instancia(puente entre los 2 archivos)
+#creamos el objeto controlador , para la clase controladorBD
+controlador=controladorBD()#de manera que controlador el la instancia o puente entre las 2 ventanas
+#Creamos 8un metodo para poder ejecutar el insert
+def ejecutainsert():#Este es el que vamos a ejecutar como comand
+    #como la variable var.... tiene un string, eso no lo pude manejar la base de datos, extraemos los datos usando un get
+    controlador.guardarusuario(varNom.get(),varCor.get(),varCon.get())#usamos el objeto/instancia controlador para ejecutar guardar ususario, y le pasamos las variables tomadas del entry de cada una
+    #como es un metodo de guardar, no regresa nada
 #paso 1
 ventana=Tk()
 ventana.title("CRUD de Usuarios")
@@ -29,7 +39,7 @@ varCon=tk.StringVar()#para vaciar el Entry
 lblCon=Label(pestaña1,text="Contraseña: ").pack()
 txtCon=Entry(pestaña1,textvariable=varCon).pack() #text variable te pide la variable donde va aguardar
 
-btnGuardar=Button(pestaña1,text="Guardar Usuario").pack()
+btnGuardar=Button(pestaña1,text="Guardar Usuario",command=ejecutainsert).pack()#Usamos el ejecutar insert para guardar la información
 
 #Paso 3
 panel.add(pestaña1,text="Agregar Usuarios")#posiciona la pestaña y le agrega el texto
--- tkintersqllite/controladorBD.py
+++ tkintersqllite/controladorBD.py
@@ -1,3 +1,46 @@
 from tkinter import messagebox
 import sqlite3
-import bcrypt
+import bcrypt #biblioteca para encriptar
+
+class controladorBD:
+    def __init__(self):
+        pass
+    def conexionBD(self):#Primer metodo para establecer conexión
+        try:
+            conexion=sqlite3.connect("C:/Users/leon_/Documents/UPQ/5° Cuatri/PROGRMACIÓN OO/Tkinter_SQLlite/tkintersqllite/BasedeDatosUsuarios.db")#Funcion de sql lite, para conectar, usa la ruta de donde esta el archivo físico
+            print("Conectado a BD")
+            return conexion#para poder conectar la base datos osea conexion carga la conexion
+        except sqlite3.OperationalError:#Para pedir que continue a pesar de errores
+            print("No se pudo Conectar")
+    def guardarusuario(self,nom,cor,con):#Metodo para guardar usuario, usa self por el encapsulado y las variables que vamos a requerir para trabajar
+        #El primer paso es mandar a llamar la conexión, guardandola en una variable llamada  conx
+        conx=self.conexionBD()
+        #2 paso, validar vacios en la informacion a guardar
+        if(nom=="" or cor=="" or con==""):
+            messagebox.showwarning("Cuidado","Formulario incompleto")
+            conx.close()#cierra la conexion evita errores, siempre que se abre, se vuelve a cerrar despues de usar
+        else:#Ahora si realizamos el inser a la base de datos
+            cursor=conx.cursor()#es la que le va a permitir ejecutar acciones a la base datos(insert), por tanto debe tner conexion a BD y usar un objeto Cursor
+            #crear una lista para evitar errores de sintxis con los para metros que insertaremos
+            conh=self.encriptarcontraseña(con)#usamos self porque es un metodo de la clase, con esto encriptamos la contraseña en con, es decir el return conha se guarda en con
+            datos=(nom,cor,conh)#Usamos el conh para guaradar la contraseña encriptada
+            #creamos la sintaxis sql para hacer el insert(lenguaje de sql).
+            sqlinsert="insert into tbRegistrados(Nombre,Correo,Contraseña) values(?,?,?)"#El signo es para decir que es pendiente el parametro porqe va por el cursor
+            #5 ejecutar insert
+            cursor.execute(sqlinsert,datos)#le pedimos al cursor ejecutar el insert con los datos guardados en la variable datos(antes definida)
+            conx.commit()#Esta funcion se usa para guardar la informacion en la base datos, la informacion proporcionadapor el cursor
+            conx.close
+            messagebox.showinfo("Exito","Usuario guardado")
+    #Creamos metodo para encriptar
+    def encriptarcontraseña(self,con):#ocupa la contraseña para encriptar
+        #contraseña plana es la contraseña sin encriptar y la guardamos en una variable
+        conplana=con
+        #pasamos la contraseña guardada en conplana a bytes usando el encode
+        conplana=conplana.encode()
+        #sal(es una varibal que puedo cambiar 'sal' permite que la contraseña sea lo más aleatora posible y la vamos a usar con la funcion bcrypt y usamos gensalt
+        sal=bcrypt.gensalt()
+        #ahora guardamos la contraseña encriptada en una variable nueva, usamos bcript, usamos la funcion hashpw mas la contraseña original y sal que es la seguridad a usar
+        conha=bcrypt.hashpw(conplana,sal)
+        print(conha)
+        #regresamos la contraseña encriptada
+        return conha
